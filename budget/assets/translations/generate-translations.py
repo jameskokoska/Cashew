6a84c8709cfaf3b53f2efda47f7fdc962ac6c130
saxc <saxc@users.noreply.github.com>
2023-12-31 23:37:54 +0100

    Update script for Linux support.

--- budget/assets/translations/generate-translations.py
+++ budget/assets/translations/generate-translations.py
@@ -1,9 +1,11 @@
+#!/usr/bin/python
+
 import csv
 import json
 import os
 import requests
 
-dir_path = os.path.dirname(os.path.realpath(__file__)) + "\\"
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 csv_file_name = 'translations.csv'
 
@@ -14,17 +16,17 @@ print("Downloading CSV...")
 response = requests.get(google_sheets_url)
 
 if response.status_code == 200:
-    with open(dir_path + csv_file_name, 'wb') as csv_file:
+    with open(os.path.join(dir_path, csv_file_name), 'wb') as csv_file:
         csv_file.write(response.content)
     print('CSV file downloaded successfully')
 else:
     print('Failed to download CSV - status code: ', response.status_code)
 
-print("Reading", dir_path + csv_file_name)
+print("Reading", os.path.join(dir_path, csv_file_name))
 
 
 # Read the CSV file
-with open(dir_path + csv_file_name, 'r', encoding='utf-8') as file:
+with open(os.path.join(dir_path, csv_file_name), 'r', encoding='utf-8') as file:
     reader = csv.reader(file)
     rows = list(reader)
 
@@ -40,7 +42,7 @@ for lang in languages:
         current_lang_data[row[0]] = row[current_lang_index]
 
     # Write the JSON file
-    with open(dir_path + "generated/" + lang + ".json", 'w', encoding='utf-8') as file:
+    with open(os.path.join(dir_path, "generated", lang + ".json"), 'w', encoding='utf-8') as file:
         json.dump(current_lang_data, file, indent=2, ensure_ascii=False)
 
     current_lang_index += 1
